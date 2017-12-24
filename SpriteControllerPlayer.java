8786aef9f75046341909e96460defa41242ced41
Ata Saeed <ata.saeed@mail.mcgill.ca>
2017-12-24 13:41:38 +0000

    Implemented SpriteController interface in SpriteControllerPlayer

--- SpriteControllerPlayer.java
+++ SpriteControllerPlayer.java
@@ -2,6 +2,19 @@
  * SpriteControllerPlayer implements SpriteController
  */
 public class SpriteControllerPlayer implements SpriteController {
+    private TextSprite sprite;
 
+    public SpriteControllerPlayer(TextSprite sprite){
+        this.sprite = sprite;
+    }
+
+    @Override
+    public void TakeTurn() {
+        
+    }   
     
+    @Override
+    public TextSprite Sprite() {
+        return this.sprite;
+    }
 }
