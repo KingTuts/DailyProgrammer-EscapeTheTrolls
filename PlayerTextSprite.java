180bbd2bc0a8006f05fdb34e9c6b784bdb353b2e
Ata Saeed <ata.saeed@mail.mcgill.ca>
2017-12-24 01:37:56 +0000

    Created Position class. Updated TextSprite interface with postion class

--- PlayerTextSprite.java
+++ PlayerTextSprite.java
@@ -2,6 +2,45 @@
  * PlayerTextSprite implements Te
  */
 public class PlayerTextSprite implements TextSprite {
+    private char upChar, downChar, leftChar, rightChar;
+    private Helper.Direction direction;
+    private Position position;
 
-    
+    public PlayerTextSprite(Position startPosition, char upChar, char downChar, char leftChar, char rightChar) {
+        this.position = startPosition;
+
+        this.upChar = upChar;
+        this.downChar = downChar;
+        this.leftChar = leftChar;
+        this.rightChar = rightChar;
+
+        this.direction = Direction.UP;
+    }
+
+    @Override
+    public Position GetPosition() {
+        return this.position;
+    }
+
+    @Override
+    public Helper.Direction Direction() {
+        return this.direction;
+    }
+
+    @Override
+    public void SetDirection(Helper.Direction direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public void Advance() {
+        
+    }
+
+    @Override
+    public char AsChar() {
+        return 0;
+    }
+
+
 }
---
+++ Position.java
@@ -0,0 +1,20 @@
+/**
+ * Position
+ */
+public class Position {
+    private int x;
+    private int y;
+    
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int X() {
+        return this.x;
+    }
+
+    public int Y() {
+        return this.y;
+    }
+}
