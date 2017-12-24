cc7a1c478c2082736c67a1c0ebcdb6fc8a014a0f
Ata Saeed <ata.saeed@mail.mcgill.ca>
2017-12-24 00:40:21 +0000

    Changed GameMap.map to char[][] and added methods for rows and length

--- GameMap.java
+++ GameMap.java
@@ -5,10 +5,15 @@ import java.io.IOException;
  */
 public class GameMap {
     private String mapString;
+    public static final char corridorChar = ' ';
+    public static final char wallChar = '#';
+    private char[][] map;
 
-    public GameMap(String mapFilePath) throws IOException {
+    public GameMap(String mapFilePath) throws IOException, Exception {
         this.mapString = GetMapString(mapFilePath);
+        this.map = GenerateMap(this.mapString);
     }
+
     /**
      * Returns string representation of map
      */
@@ -16,7 +21,51 @@ public class GameMap {
         return this.mapString;
     }
 
+    public char[][] Map(){
+        return this.map;
+    }
+
+    public int Rows(){
+        return this.map.length;
+    }
+
+    public int Columns(){
+        return this.map[0].length;
+    }
+
+    
+
     private static String GetMapString(String mapPath) throws IOException {
         return Helper.ReadTextFile(mapPath);
     }
+
+    private static char[][] GenerateMap(String mapString) throws Exception {
+
+        String[] rowStrings = mapString.split("\n");
+
+        char[][] map = new char[rowStrings.length][(rowStrings[0]).length()];
+
+        for (int row = 0; row < map.length; row++) {
+            for (int col = 0; col < map[0].length; col++) {
+                switch (rowStrings[row].charAt(col)) {
+                case wallChar:
+                    map[row][col] = wallChar;
+                    break;
+                case corridorChar:
+                    map[row][col] = wallChar;
+                    break;
+                default:
+                    throw new Exception("Invalid map char:" + rowStrings[row].charAt(col) + "\n\tValid map chars: "
+                            + wallChar + ", " + corridorChar);
+                }
+            }
+        }
+
+        return map;
+    }
+
+    public enum BlockType {
+        WALL, CORRIDOR
+    }
+
 }
