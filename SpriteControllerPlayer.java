b30cb35bca3cabc838492d422f7e107504bdbb8c
Ata Saeed <ata.saeed@mail.mcgill.ca>
2018-01-01 19:38:41 +0000

    Started implementtion of moving with key presses

--- SpriteControllerPlayer.java
+++ SpriteControllerPlayer.java
@@ -1,24 +1,38 @@
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
 /**
  * SpriteControllerPlayer implements SpriteController
  */
 public class SpriteControllerPlayer implements SpriteController {
     private TextSprite sprite;
     private KeyPressedEmitter kpEmitter;
+    private List<TextSpriteActions> actionQueue;
 
-    public SpriteControllerPlayer(TextSprite sprite, KeyPressedEmitter kpEmitter){
+    public SpriteControllerPlayer(TextSprite sprite, KeyPressedEmitter kpEmitter) {
         this.sprite = sprite;
         this.kpEmitter = kpEmitter;
+        this.actionQueue = new ArrayList<TextSpriteActions>();
 
-        this.kpEmitter.RegisterKeyListener();
+
+        // Register key bindings
+        this.kpEmitter.RegisterKeyBinding(keyStroke, keyString, func);
+
     }
 
     @Override
     public void TakeTurn() {
         // TODO: Add blocking key listeners here
-    }   
-    
+    }
+
     @Override
     public TextSprite Sprite() {
         return this.sprite;
+    }
+
+
+
+    private enum TextSpriteActions {
+        TURN_UP, TURN_DOWN, TURN_LEFT, TURN_RIGHT, MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT
     }
 }
