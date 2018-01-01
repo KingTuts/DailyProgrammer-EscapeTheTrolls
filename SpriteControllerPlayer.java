2d66fe10620be23ce7aa04ba86034ef2d8ed5130
Ata Saeed <ata.saeed@mail.mcgill.ca>
2018-01-01 14:45:35 +0000

    Started KeyPressedEmitter implementation

--- SpriteControllerPlayer.java
+++ SpriteControllerPlayer.java
@@ -3,9 +3,13 @@
  */
 public class SpriteControllerPlayer implements SpriteController {
     private TextSprite sprite;
+    private KeyPressedEmitter kpEmitter;
 
-    public SpriteControllerPlayer(TextSprite sprite){
+    public SpriteControllerPlayer(TextSprite sprite, KeyPressedEmitter kpEmitter){
         this.sprite = sprite;
+        this.kpEmitter = kpEmitter;
+
+        this.kpEmitter.RegisterKeyListener();
     }
 
     @Override
